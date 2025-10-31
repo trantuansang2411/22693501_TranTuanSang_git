a74687d064d1a02210355fb2f7dd54059bf73db3
trantuansang2411 <22693501.sang@student.iuh.edu.vn>
2025-10-31 12:53:59 -0700

    Success

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -93,10 +93,10 @@ class ProductController {
   }
   async getProductById(req, res) {
     try {
-      const {id}=req.params
+      const {id}=req.params;
       const product = await this.productService.getProductById(id);
       if (!product) {
-        return res.status(404).json({ message: "Product isn't exit" });
+        return res.status(404).json({ message: "Not Found" });
       }
       res.status(200).json(product);
     } catch (error) {
